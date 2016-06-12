294d230b15abbe6167e12a145080b3459971b343
chrisruk <chrisrichardsonuk@gmail.com>
2016-06-12 12:20:20 +0100

    AMC SCF module

---
+++ python/scf.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+# 
+# Copyright 2016 <+YOU OR YOUR COMPANY+>.
+# 
+# This is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+# 
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this software; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street,
+# Boston, MA 02110-1301, USA.
+# 
+
+import numpy
+from gnuradio import gr
+
+class scf(gr.sync_block):
+    """
+    docstring for block scf
+    """
+    def __init__(self):
+        gr.sync_block.__init__(self,
+            name="scf",
+            in_sig=[<+numpy.float+>],
+            out_sig=[<+numpy.float+>])
+
+
+    def work(self, input_items, output_items):
+        in0 = input_items[0]
+        out = output_items[0]
+        # <+signal processing here+>
+        out[:] = in0
+        return len(output_items[0])
+
