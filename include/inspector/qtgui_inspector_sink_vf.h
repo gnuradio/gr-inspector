963467501d637c7b48a48ece76f20799cd292485
Sebastian Müller <senpo@posteo.de>
2016-06-15 16:50:56 +0200

    Add doxygen documentation to header files

--- include/inspector/qtgui_inspector_sink_vf.h
+++ include/inspector/qtgui_inspector_sink_vf.h
@@ -1,17 +1,17 @@
 /* -*- c++ -*- */
-/* 
+/*
  * Copyright 2016 <+YOU OR YOUR COMPANY+>.
- * 
+ *
  * This is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3, or (at your option)
  * any later version.
- * 
+ *
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this software; see the file COPYING.  If not, write to
  * the Free Software Foundation, Inc., 51 Franklin Street,
@@ -34,9 +34,22 @@ namespace gr {
   namespace inspector {
 
     /*!
-     * \brief <+description of block+>
+     * \brief GUI for the inspector toolkit
      * \ingroup inspector
      *
+     * \details
+     * This GUI uses QT and QWT to create a graphical interface to control the
+     * inspector and get feedback about the detected and analyzed signals.
+     *
+     * The PSD estimation of the Signal Detector block gets displayed here along
+     * with signal markers and informations. Manual signal selection can be done
+     * within the GUI created by this block.
+     *
+     * Message ports:
+     * - map in: takes RF map from Signal Detector block and visualizes the included
+     * information in the plot
+     * - map out: passes either the input message when manual selection is deactivated
+     * or passes RF map according to the user's settings.
      */
     class INSPECTOR_API qtgui_inspector_sink_vf : virtual public gr::sync_block
     {
@@ -44,12 +57,11 @@ namespace gr {
       typedef boost::shared_ptr<qtgui_inspector_sink_vf> sptr;
 
       /*!
-       * \brief Return a shared_ptr to a new instance of inspector::qtgui_inspector_sink_vf.
+       * \brief Build a GUI sink instance.
        *
-       * To avoid accidental use of raw pointers, inspector::qtgui_inspector_sink_vf's
-       * constructor is in a private implementation
-       * class. inspector::qtgui_inspector_sink_vf::make is the public interface for
-       * creating new instances.
+       * \param samp_rate Sample rate of the input signal.
+       * \param fft_len Vector input size and number of PSD sample points
+       * \param cfreq Center frequency of source. Set to 0 if only relative frequencies are desired downstream.
        */
       static sptr make(double samp_rate, int fft_len, float cfreq = 0, QWidget *parent = NULL);
 
@@ -64,4 +76,3 @@ namespace gr {
 } // namespace gr
 
 #endif /* INCLUDED_INSPECTOR_QTGUI_INSPECTOR_SINK_VF_H */
-
--- include/inspector/signal_extractor_c.h
+++ include/inspector/signal_extractor_c.h
@@ -1,17 +1,17 @@
 /* -*- c++ -*- */
-/* 
+/*
  * Copyright 2016 <+YOU OR YOUR COMPANY+>.
- * 
+ *
  * This is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3, or (at your option)
  * any later version.
- * 
+ *
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this software; see the file COPYING.  If not, write to
  * the Free Software Foundation, Inc., 51 Franklin Street,
@@ -29,9 +29,15 @@ namespace gr {
   namespace inspector {
 
     /*!
-     * \brief <+description of block+>
+     * \brief Extractor block to extract signal samples out of bundled message passed from Signal Separator block.
      * \ingroup inspector
      *
+     * \details
+     * Signal Extractor block is able to extract signal samples for specified
+     * signal out of message passed by Signal Separator block. These messages
+     * contain the samples of all detected signals. This block only passes the
+     * ones beloging to the specified signal number as a complex stream. This
+     * way, custom signal chains can be appended after the inspector blocks.
      */
     class INSPECTOR_API signal_extractor_c : virtual public gr::sync_block
     {
@@ -39,12 +45,9 @@ namespace gr {
       typedef boost::shared_ptr<signal_extractor_c> sptr;
 
       /*!
-       * \brief Return a shared_ptr to a new instance of inspector::signal_extractor_c.
+       * \brief Return a new Extractor block instance
        *
-       * To avoid accidental use of raw pointers, inspector::signal_extractor_c's
-       * constructor is in a private implementation
-       * class. inspector::signal_extractor_c::make is the public interface for
-       * creating new instances.
+       * \param signal Output Signal number (beginning with 0) for desired samples
        */
       static sptr make(int signal);
     };
@@ -53,4 +56,3 @@ namespace gr {
 } // namespace gr
 
 #endif /* INCLUDED_INSPECTOR_SIGNAL_EXTRACTOR_C_H */
-
