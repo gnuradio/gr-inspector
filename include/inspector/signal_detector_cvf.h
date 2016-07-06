1713539746b02fcb69211f90955ddbbfe09a527f
Sebastian Müller <senpo@posteo.de>
2016-07-06 15:33:25 +0200

    Copyright changes

--- include/inspector/signal_detector_cvf.h
+++ include/inspector/signal_detector_cvf.h
@@ -1,6 +1,6 @@
 /* -*- c++ -*- */
 /*
- * Copyright 2016 <+YOU OR YOUR COMPANY+>.
+ * Copyright 2016 Sebastian Müller.
  *
  * This is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
