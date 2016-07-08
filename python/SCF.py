cf17807b69808bb337d0b682b91dc4d832482b44
chrisruk <chrisrichardsonuk@gmail.com>
2016-07-08 22:00:48 +0100

    SCF block

---
+++ python/SCF.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+# 
+# Copyright 2016 <+YOU OR YOUR COMPANY+>.
+# 
+# This is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+# 
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this software; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street,
+# Boston, MA 02110-1301, USA.
+# 
+
+import numpy
+from gnuradio import gr
+import collections
+import numpy as np
+import math
+# Generate 2D array of SCF data
+def scf(y):
+
+    za = []
+    d = collections.deque(maxlen=10)
+    y = y[0:1024*5]
+    N = 100#3000             # Number of frames
+    T = int(len(y) / N) # Frame length
+    #print("Flen",T)
+    Fs = T #*2
+    al = 1*Fs
+    n = 0
+
+    frame = y[(n*int(T)):int(n*T)+int(T)]
+    xf = np.fft.fftshift(np.fft.fft(frame))
+    xfp = np.append([0]*int(al/2),xf)
+    xfm = np.append(xf,[0]*int(al/2))
+    Sxf = (1/T) * xfp * np.conj(xfm)
+    Sxf = Sxf * (np.e**-(1j*2*np.pi*al*(N*T)))
+    mx = len(Sxf)
+    alph = []
+
+    for a in np.arange(0,1,0.1):
+
+
+        Fs = T #*2
+        al = a * Fs
+        alph.append(a)
+    
+        out = []
+
+        count = 0
+        for n in range(0,N):
+
+            count = n
+            frame = y[int(n*T):int(n*T)+T]
+
+            xf = np.fft.fftshift(np.fft.fft(frame))
+            
+            xfp = np.append([0]*int(al/2),xf)
+            xfm = np.append(xf,[0]*int(al/2))
+            np.set_printoptions(threshold=np.nan)
+            
+            # removed 1/T 
+            Sxf =  xfp * np.conj(xfm) 
+            Sxf = Sxf * (np.e**-(1j*2*np.pi*al*(count*T)))
+    
+            orig = len(Sxf)
+            Sxf.resize((mx,))
+            newsize = len(Sxf)
+
+            Sxf = np.roll(Sxf,int((newsize-orig)/2))
+
+            new = []
+            for v in Sxf:
+                new.append(math.sqrt(v.imag**2+v.real**2))
+        
+            out.append(new)
+    
+        tm = np.mean( np.array(out), axis=0 )
+        d.append(tm)
+
+        # mean of columns
+        smoothed = np.mean(np.array(d),axis=0)
+
+        za.append(smoothed)
+ 
+    out = np.array(za) 
+    o = (out/out.max())
+    o[o == np.inf] = 0
+
+    return o.flatten()
+
+
+class SCF(gr.sync_block):
+    """
+    docstring for block SCF
+    """
+    def __init__(self):
+        gr.sync_block.__init__(self,
+            name="SCF",
+            in_sig=[(numpy.complex64,1024*5)],
+            out_sig=[(numpy.float32,760)])
+
+
+    def work(self, input_items, output_items):
+        outp = []
+        out = output_items[0]
+
+        for i in input_items:
+            for v in i:
+                outp.append(scf(v))
+
+        out[:] = outp
+        return len(outp)
+
