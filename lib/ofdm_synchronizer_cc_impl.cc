b03b065739e6a99c57a7b9aff306800150262f88
Sebastian Müller <senpo@posteo.de>
2016-07-29 15:30:52 +0200

    [sync] add cpp implementation

---
+++ lib/ofdm_synchronizer_cc_impl.cc
@@ -0,0 +1,132 @@
+/* -*- c++ -*- */
+/* 
+ * Copyright 2016 Sebastian Müller.
+ * 
+ * This is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ * 
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this software; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <gnuradio/io_signature.h>
+#include "ofdm_synchronizer_cc_impl.h"
+#include <volk/volk.h>
+#include <complex>
+
+namespace gr {
+  namespace inspector {
+
+    ofdm_synchronizer_cc::sptr
+    ofdm_synchronizer_cc::make(double samp_rate)
+    {
+      return gnuradio::get_initial_sptr
+        (new ofdm_synchronizer_cc_impl(samp_rate));
+    }
+
+    /*
+     * The private constructor
+     */
+    ofdm_synchronizer_cc_impl::ofdm_synchronizer_cc_impl(double samp_rate)
+      : gr::sync_block("ofdm_synchronizer_cc",
+              gr::io_signature::make(1, 1, sizeof(gr_complex)),
+              gr::io_signature::make(1, 1, sizeof(gr_complex)))
+    {
+      d_samp_rate = samp_rate;
+      d_fft_len = 0;
+      d_cp_len = 0;
+      d_msg_received = false;
+
+      // message port for parameter estimations
+      message_port_register_in(pmt::intern("ofdm_in"));
+      set_msg_handler(pmt::intern("ofdm_in"), boost::bind(
+              &ofdm_synchronizer_cc_impl::handle_msg, this, _1));
+    }
+
+    /*
+     * Our virtual destructor.
+     */
+    ofdm_synchronizer_cc_impl::~ofdm_synchronizer_cc_impl()
+    {
+    }
+
+    void
+    ofdm_synchronizer_cc_impl::handle_msg(pmt::pmt_t msg) {
+      // get FFT and CP length out of parameter estmation msg
+      d_fft_len = (int)pmt::to_float(pmt::tuple_ref(pmt::tuple_ref(msg, 3), 1));
+      d_cp_len = (int)pmt::to_float(pmt::tuple_ref(pmt::tuple_ref(msg, 4), 1));
+      d_msg_received = true;
+    }
+
+    std::vector<gr_complex>
+    ofdm_synchronizer_cc_impl::autocorr(const gr_complex *in, int len) {
+      std::vector<gr_complex> result;
+      gr_complex temp[len-d_fft_len];
+      gr_complex Rxx;
+      volk_32fc_x2_multiply_conjugate_32fc(temp, &in[d_fft_len], in, len-d_fft_len);
+
+      for(int i = 0; i < len-d_fft_len-d_cp_len; i++) {
+        Rxx = gr_complex(0,0);
+        for(int k = 0; k < d_cp_len; k++) {
+          Rxx += temp[i+k];
+        }
+        result.push_back(Rxx);
+      }
+      return result;
+    }
+
+    int
+    ofdm_synchronizer_cc_impl::work(int noutput_items,
+        gr_vector_const_void_star &input_items,
+        gr_vector_void_star &output_items)
+    {
+      const gr_complex *in = (const gr_complex *) input_items[0];
+      gr_complex *out = (gr_complex *) output_items[0];
+      if(!d_msg_received) {
+        return noutput_items;
+      }
+
+      if(noutput_items < 4096) {
+        return 0;
+      }
+
+      // Do <+signal processing+>
+      std::vector<gr_complex> r = autocorr(in, noutput_items);
+      float r_mag[noutput_items-d_fft_len-d_cp_len];
+      volk_32fc_magnitude_32f(r_mag, &r[0], noutput_items-d_fft_len-d_cp_len);
+      std::vector<float> r_vec(r_mag, r_mag+noutput_items-d_fft_len-d_cp_len);
+      int k = std::distance(r_vec.begin(), std::max_element(r_vec.begin(), r_vec.begin()+d_fft_len+d_cp_len));
+      float n = std::arg(r[k]);
+      d_rotator.set_phase_incr(std::exp(gr_complex(0,-n/d_fft_len)));
+
+      std::cout << "n = " << n*d_samp_rate/(2*d_fft_len*M_PI) << std::endl;
+
+      for(int i = 0; i < noutput_items; i++) {
+        out[i] = d_rotator.rotate(in[i]);
+      }
+      int pos = k;
+      while(pos < nitems_written(0)+noutput_items) {
+        add_item_tag(0, pos, pmt::intern("symbol"), pmt::from_long(d_fft_len+d_cp_len));
+        pos += d_fft_len+d_cp_len;
+      }
+
+      // Tell runtime system how many output items we produced.
+      return noutput_items;
+    }
+
+  } /* namespace inspector */
+} /* namespace gr */
+
---
+++ lib/ofdm_synchronizer_cc_impl.h
@@ -0,0 +1,55 @@
+/* -*- c++ -*- */
+/* 
+ * Copyright 2016 Sebastian Müller.
+ * 
+ * This is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ * 
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this software; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#ifndef INCLUDED_INSPECTOR_OFDM_SYNCHRONIZER_CC_IMPL_H
+#define INCLUDED_INSPECTOR_OFDM_SYNCHRONIZER_CC_IMPL_H
+
+#include <inspector/ofdm_synchronizer_cc.h>
+#include <gnuradio/blocks/rotator.h>
+
+namespace gr {
+  namespace inspector {
+
+    class ofdm_synchronizer_cc_impl : public ofdm_synchronizer_cc
+    {
+     private:
+      double d_samp_rate;
+      int d_fft_len, d_cp_len;
+      bool d_msg_received;
+      blocks::rotator d_rotator;
+
+     public:
+      ofdm_synchronizer_cc_impl(double samp_rate);
+      ~ofdm_synchronizer_cc_impl();
+
+      void handle_msg(pmt::pmt_t msg);
+      std::vector<gr_complex> autocorr(const gr_complex *in, int len);
+
+      // Where all the action really happens
+      int work(int noutput_items,
+         gr_vector_const_void_star &input_items,
+         gr_vector_void_star &output_items);
+    };
+
+  } // namespace inspector
+} // namespace gr
+
+#endif /* INCLUDED_INSPECTOR_OFDM_SYNCHRONIZER_CC_IMPL_H */
+
