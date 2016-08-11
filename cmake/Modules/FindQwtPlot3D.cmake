690070353600d18864ba366ba645e15d58eca71f
chrisruk <chrisrichardsonuk@gmail.com>
2016-08-11 16:11:49 +0100

    cmake

--- cmake/Modules/FindQwtPlot3D.cmake
+++ cmake/Modules/FindQwtPlot3D.cmake
@@ -3,12 +3,12 @@
 #  QWTPLOT3D_LIBRARIES, the libraries to link against
 #  QWTPLOT3D_FOUND, If false, do not try to use GLUT.
 
-FIND_PATH( QWTPLOT3D_INCLUDE_DIR qwt3d_plot.h /usr/include/ /usr/local/include /usr/include/qwtplot3d-qt4/ /usr/local/include/qwtplot3d-qt4 )
+FIND_PATH( QT_QWTPLOT3D_INCLUDE_DIR qwt3d_plot.h /usr/include/ /usr/local/include /usr/include/qwtplot3d/ /usr/include/qwtplot3d-qt4/ /usr/local/include/qwtplot3d-qt4 )
 
 #MESSAGE( STATUS "${QWTPLOT3D_INCLUDE_DIR}")
-FIND_LIBRARY( QWTPLOT3D_LIBRARIES qwtplot3d-qt4 /usr/lib/ usr/local/lib ) 
+FIND_LIBRARY( QT_QWTPLOT3D_LIBRARY qwtplot3d qwtplot3d-qt4 /usr/lib/ usr/local/lib ) 
 #MESSAGE( STATUS "${QWTPLOT3D_LIBRARIES}")
-IF ( QWTPLOT3D_INCLUDE_DIR  AND QWTPLOT3D_LIBRARIES ) 
+IF ( QT_QWTPLOT3D_INCLUDE_DIR  AND QWTPLOT3D_LIBRARIES ) 
   SET( QWTPLOT3D_FOUND TRUE ) 
 ENDIF ( QWTPLOT3D_INCLUDE_DIR  AND QWTPLOT3D_LIBRARIES ) 
 
