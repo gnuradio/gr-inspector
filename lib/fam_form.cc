c5a9e25e0480c80bb0d5f07e8b83893105519258
chrisruk <chrisrichardsonuk@gmail.com>
2016-08-13 23:29:46 +0100

    tidy

--- lib/fam_form.cc
+++ lib/fam_form.cc
@@ -32,129 +32,50 @@
 namespace Qwt3D
 {
 
-/*
-void Plot::adjustScales(){
-    ParallelEpiped hull = this->hull();
+    void Plot::calculateHull()
+    {
+	    if (actualData_p->empty())
+		    return;
 
-    int d = (hull.maxVertex - hull.minVertex).length();
-        
-    double xxScale = 0.5;
-    double yScale = 0.5;
-    double zScale = 0.5;
-    setScale(xxScale, yScale, zScale);
+        ParallelEpiped hull(Triple(DBL_MAX,DBL_MAX,DBL_MAX),Triple(-DBL_MAX,-DBL_MAX,-DBL_MAX));
 
-    setZoom(d / (2*sqrt(3.0)));
+        hull.minVertex.x = 0;
+        hull.maxVertex.x = 10;
 
-    coordinates()->axes[Z1].setTicLength(3*xxScale, xxScale);
-    coordinates()->axes[Z2].setTicLength(3*xxScale, xxScale);
-    coordinates()->axes[Z3].setTicLength(3*xxScale, xxScale);
-    coordinates()->axes[Z4].setTicLength(3*xxScale, xxScale);
+        hull.minVertex.y = 0;
+        hull.maxVertex.y = 10;
 
-    coordinates()->axes[X1].setTicLength(3*yScale, yScale);
-    coordinates()->axes[X2].setTicLength(3*yScale, yScale);
-    coordinates()->axes[X3].setTicLength(3*yScale, yScale);
-    coordinates()->axes[X4].setTicLength(3*yScale, yScale);
-
-    coordinates()->axes[Y1].setTicLength(3*xxScale, xxScale);
-    coordinates()->axes[Y2].setTicLength(3*xxScale, xxScale);
-    coordinates()->axes[Y3].setTicLength(3*xxScale, xxScale);
-    coordinates()->axes[Y4].setTicLength(3*xxScale, xxScale);
-
-}
-*/
-
-
-
-
-
-
-
-
-void Plot::calculateHull()
-{
-	if (actualData_p->empty())
-		return;
-
-
-ParallelEpiped hull(Triple(DBL_MAX,DBL_MAX,DBL_MAX),Triple(-DBL_MAX,-DBL_MAX,-DBL_MAX));
-
-
-
-hull.minVertex.x = 0;
-hull.maxVertex.x = 10;
-
-hull.minVertex.y = 0;
-hull.maxVertex.y = 10;
-
-hull.minVertex.z = 0;
-hull.maxVertex.z = 10;
-/*
-	*	if (data[i].x < hull.minVertex.x)
-			hull.minVertex.x = data[i].x;
-		if (data[i].y < hull.minVertex.y)
-			hull.minVertex.y = data[i].y;
-		if (data[i].z < hull.minVertex.z)
-			hull.minVertex.z = data[i].z;
+        hull.minVertex.z = 0;
+        hull.maxVertex.z = 10;
 	
-		if (data[i].x > hull.maxVertex.x)
-			hull.maxVertex.x = data[i].x;
-		if (data[i].y > hull.maxVertex.y)
-			hull.maxVertex.y = data[i].y;
-		if (data[i].z > hull.maxVertex.z)
-			hull.maxVertex.z = data[i].z;*/
-
-//	actualDataC_->setHull(hull);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	setHull(hull);
-}
+        setHull(hull);
+    }
 
     Plot::Plot()
     {
-        setTitle("A Simple SurfacePlot Demonstration");
-
+        //setTitle("A Simple SurfacePlot Demonstration");
         /*Rosenbrock rosenbrock(this);
-
         rosenbrock.setMesh(41,31);
         rosenbrock.setDomain(-1.73,1.5,-1.5,1.5);
         rosenbrock.setMinZ(-10);
-
         rosenbrock.create();*/
 
         setRotation(30,0,15);
         setScale(1,1,1);
-       // setShift(0.15,0,0);
+        // setShift(0.15,0,0);
         setZoom(0.9);
         showNormals(false);
-/*
+        /*
         for (unsigned i=0; i!=coordinates()->axes.size(); ++i)
         {
             coordinates()->axes[i].setMajors(7);
             coordinates()->axes[i].setMinors(4);
         }
-
-
         coordinates()->axes[X1].setLabelString("x-axis");
         coordinates()->axes[Y1].setLabelString("y-axis");
         coordinates()->axes[Z1].setLabelString(QChar (0x38f)); // Omega - see http://www.unicode.org/charts/
-
-
         setCoordinateStyle(BOX);
-*/
+        */
         updateData();
         updateGL();
     }
@@ -175,8 +96,6 @@ namespace gr
             layout.addWidget(plot);
             plot->resize(1024,1024);
 
-           // double data[10][10] = { {0.0} }; // all elements initialized to 0.
-
             unsigned int columns = 10;
             unsigned int rows = 10;
 
@@ -184,12 +103,6 @@ namespace gr
             for(int i = 0; i < rows; ++i)
                 dat[i] = new double[columns];
                 
-            for(int y=0;y<columns;y++){
-            for(int x=0;x<rows;x++){
-                dat[x][y] = 1.3;
-            }   
-            }
-
             double minx=0.0,maxx=10.0,miny=0,maxy=10.0;
             plot->loadFromData 	(   dat,
 			                        columns,
@@ -247,26 +160,15 @@ namespace gr
             for (int i=0; i<4*3;i++)
                     plot->coordinates()->axes[i].setAutoScale(false);
 
-
-
-/*
-        plot->setRotation(30,0,15);
-        plot->setScale(1,1,1);
-       // setShift(0.15,0,0);
-    plot->setIsolines(0);
-        plot->setZoom(0.9);
-        plot->showNormals(false);
-*/
+            /*
+            plot->setRotation(30,0,15);
+            plot->setScale(1,1,1);
+            setShift(0.15,0,0);
+            plot->setIsolines(0);
+            plot->setZoom(0.9);
+            plot->showNormals(false);
+            */
             plot->repaint();
-
-
-
-
-
-
-
-
-
             repaint();
         }
 
