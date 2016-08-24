7730eac7a50d488340bf7c9719b9e82fa40c2b79
chrisruk <chrisrichardsonuk@gmail.com>
2016-08-24 14:17:07 +0100

    test

--- lib/qa_vis3d.h
+++ lib/qa_vis3d.h
@@ -1,6 +1,6 @@
 /* -*- c++ -*- */
 /* 
- * Copyright 2016 <+YOU OR YOUR COMPANY+>.
+ * Copyright 2016 Christopher Richardson
  * 
  * This is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
--- python/qa_vis3d.py
+++ python/qa_vis3d.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # 
-# Copyright 2016 <+YOU OR YOUR COMPANY+>.
+# Copyright 2016 Christopher Richardson
 # 
 # This is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
