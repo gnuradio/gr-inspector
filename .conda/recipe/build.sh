3e0753a14c2a17013ccaec0e5724fc2b8aed2061
Ryan Volz <ryan.volz@gmail.com>
2022-09-19 14:35:15 -0400

    Increase ctest timeout, to help pass linux-aarch64?

--- .conda/recipe/build.sh
+++ .conda/recipe/build.sh
@@ -18,5 +18,5 @@ cmake --build . --config Release -- -j${CPU_COUNT}
 cmake --build . --config Release --target install
 
 if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
-    ctest --build-config Release --output-on-failure --timeout 120 -j${CPU_COUNT}
+    ctest --build-config Release --output-on-failure --timeout 300 -j${CPU_COUNT}
 fi
